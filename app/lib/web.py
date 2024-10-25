53995b8bf14c2f0cddeadc40e0b7922a4b062c2c
Patrick <patrick@coffey.app>
2024-10-26 01:19:19 +0200

    improve header parsing and add Cloudflare Access support

--- app/lib/web.py
+++ app/lib/web.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 from urllib.parse import urljoin
 
 def saveResponse(fileName, contentsRaw):
@@ -8,14 +9,22 @@ def saveResponse(fileName, contentsRaw):
         output_file.write(contents)
         output_file.close()
 
-def get(host, path, printMe=False, ignoreErrors=False, session=None):
-    fullurl = urljoin("https://{}".format(host), path)
+def get(host, path, headers={}, cloudflareAccess=False, printMe=False, ignoreErrors=False, session=None):
+    if "https://" in host or "http://" in host:
+        fullurl = urljoin(host, path)
+    else:
+        fullurl = urljoin("https://{}".format(host), path)
     if printMe:
         print("Trying: {}".format(fullurl))
     if session == None:
         s = requests.Session()
     else:
         s = session
+    for k in headers.keys():
+        s.headers[k] = headers[k]
+    if cloudflareAccess:
+        s.headers["CF-Access-Client-Id"] = os.environ["CFClientID"]
+        s.headers["CF-Access-Client-Secret"] = os.environ["CFClientSecret"]
     result = s.get(fullurl)
     if printMe:
         print(str(result.text))
@@ -26,8 +35,8 @@ def get(host, path, printMe=False, ignoreErrors=False, session=None):
             return False
         print("Error when retrieving info from {}".format(host))
         print("Status: " + str(result.status_code))
-        print("Attempted to get " + str(path))
-        responseName = "error-{}{}.json".format(result.status_code, path.replace("/", "."))
+        print("Attempted to get {}".format(fullurl))
+        responseName = "error-{}{}.json".format(result.status_code, str(fullurl.replace("https:", "")).replace("/", "."))
         try:
             responseDict = json.loads(result.text)
             responseDict["path called"] = path
@@ -37,34 +46,53 @@ def get(host, path, printMe=False, ignoreErrors=False, session=None):
         print("Error response saved to {}".format(responseName))
         return False
 
-def options(host, path, printMe=False, session=None):
-    fullurl = urljoin("https://{}".format(host), path)
+def options(host, path, headers={}, cloudflareAccess=False, printMe=False, session=None):
+    if "https://" in host or "http://" in host:
+        fullurl = urljoin(host, path)
+    else:
+        fullurl = urljoin("https://{}".format(host), path)
     if printMe:
         print("Trying: {}".format(fullurl))
     if session == None:
         s = requests.Session()
     else:
         s = session
+    for k in headers.keys():
+        s.headers[k] = headers[k]
+    if cloudflareAccess:
+        s.headers["CF-Access-Client-Id"] = os.environ["CFClientID"]
+        s.headers["CF-Access-Client-Secret"] = os.environ["CFClientSecret"]
     result = s.options(fullurl)
     if printMe:
         print("Result: {}".format(result.status_code))
     return result
     
-def head(host, path, printMe=False, session=None):
-    fullurl = urljoin("https://{}".format(host), path)
+def head(host, path, headers={}, cloudflareAccess=False, printMe=False, session=None):
+    if "https://" in host or "http://" in host:
+        fullurl = urljoin(host, path)
+    else:
+        fullurl = urljoin("https://{}".format(host), path)
     if printMe:
         print("Trying: {}".format(fullurl))
     if session == None:
         s = requests.Session()
     else:
         s = session
+    for k in headers.keys():
+        s.headers[k] = headers[k]
+    if cloudflareAccess:
+        s.headers["CF-Access-Client-Id"] = os.environ["CFClientID"]
+        s.headers["CF-Access-Client-Secret"] = os.environ["CFClientSecret"]
     result = s.head(fullurl)
     if printMe:
         print("Result: {}".format(result.status_code))
     return result
 
-def post(host, path, contentType, body={}, printMe=False, session=None):
-    fullurl = urljoin("https://{}".format(host), path)
+def post(host, path, contentType, body={}, headers={}, cloudflareAccess=False, printMe=False, session=None):
+    if "https://" in host or "http://" in host:
+        fullurl = urljoin(host, path)
+    else:
+        fullurl = urljoin("https://{}".format(host), path)
     if printMe:
         print("Trying: {}".format(fullurl))
     if session == None:
@@ -72,6 +100,11 @@ def post(host, path, contentType, body={}, printMe=False, session=None):
     else:
         s = session
     s.headers["Content-Type"] = contentType
+    for k in headers.keys():
+        s.headers[k] = headers[k]
+    if cloudflareAccess:
+        s.headers["CF-Access-Client-Id"] = os.environ["CFClientID"]
+        s.headers["CF-Access-Client-Secret"] = os.environ["CFClientSecret"]
     
     result = s.post(fullurl, data=body)
     if printMe:
@@ -79,7 +112,7 @@ def post(host, path, contentType, body={}, printMe=False, session=None):
     if result.status_code > 199 and result.status_code < 300:
         return result
     else:
-        print("Error when retrieving info from Akamai")
+        print("Error when retrieving info from {}".format(host))
         print("Status: " + str(result.status_code))
         print("Attempted to get " + str(path))
         responseName = "error-{}{}.json".format(result.status_code, host, path.replace("/", "."))
